f9d6258fb7b93ea2e57f6894548eca360554cbb3
Jorge Schrauwen <sjorge@blackdot.be>
2015-05-22 21:14:30 +0000

    revert to original patch for SABnzbd.py

--- sabnzbd/patches/patch-SABnzbd.py
+++ sabnzbd/patches/patch-SABnzbd.py
@@ -1,6 +1,8 @@
 $NetBSD$
 
---- SABnzbd.py.orig	2015-05-22 20:46:07.943361541 +0000
+Fix absolute paths.
+
+--- SABnzbd.py.orig	2013-09-02 20:06:57.000000000 +0000
 +++ SABnzbd.py
 @@ -41,6 +41,9 @@ except:
      print "The Python module Cheetah is required"
@@ -12,3 +14,12 @@ $NetBSD$
  import cherrypy
  if not cherrypy.__version__.startswith("3.2"):
      print "Sorry, requires Python module Cherrypy 3.2 (use the included version)"
+@@ -1010,7 +1013,7 @@ def main():
+ 
+     sabnzbd.MY_FULLNAME = os.path.normpath(os.path.abspath(sabnzbd.MY_FULLNAME))
+     sabnzbd.MY_NAME = os.path.basename(sabnzbd.MY_FULLNAME)
+-    sabnzbd.DIR_PROG = os.path.dirname(sabnzbd.MY_FULLNAME)
++    sabnzbd.DIR_PROG = '%%PREFIX%%/share/sabnzbdplus'
+     sabnzbd.DIR_INTERFACES = real_path(sabnzbd.DIR_PROG, DEF_INTERFACES)
+     sabnzbd.DIR_LANGUAGE = real_path(sabnzbd.DIR_PROG, DEF_LANGUAGE)
+     org_dir = os.getcwd()
