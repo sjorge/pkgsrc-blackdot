7d66bf8a023ffe176d347df7e9dd447d6a163e8d
Jorge Schrauwen <sjorge@blackdot.be>
2015-05-22 22:53:28 +0000

    updated patch for SickBeard.py

--- sickbeard/patches/patch-SickBeard.py
+++ sickbeard/patches/patch-SickBeard.py
@@ -1,6 +1,6 @@
 $NetBSD$
 
---- SickBeard.py.orig	2015-05-22 22:30:19.716084585 +0000
+--- SickBeard.py.orig	2015-05-22 22:51:55.332104723 +0000
 +++ SickBeard.py
 @@ -22,6 +22,10 @@ import sys
  if sys.version_info < (2, 5):
@@ -13,3 +13,12 @@ $NetBSD$
  try:
      import Cheetah
      if Cheetah.Version[0] != '2':
+@@ -170,7 +174,7 @@ def main():
+     # do some preliminary stuff
+     sickbeard.MY_FULLNAME = os.path.normpath(os.path.abspath(sys.argv[0]))
+     sickbeard.MY_NAME = os.path.basename(sickbeard.MY_FULLNAME)
+-    sickbeard.PROG_DIR = os.path.dirname(sickbeard.MY_FULLNAME)
++    sickbeard.PROG_DIR = '%%PREFIX%%/share/sickbeard'
+     sickbeard.DATA_DIR = sickbeard.PROG_DIR
+     sickbeard.MY_ARGS = sys.argv[1:]
+     sickbeard.DAEMON = False
