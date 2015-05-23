b205a21f9d665922d9290c6707a3cc34ffce89f6
Jorge Schrauwen <sjorge@blackdot.be>
2015-05-23 01:03:41 +0000

    couchpotato patch v3

--- couchpotato/patches/patch-CouchPotato.py
+++ couchpotato/patches/patch-CouchPotato.py
@@ -1,13 +1,18 @@
 $NetBSD$
 
---- CouchPotato.py.orig	2015-05-23 00:26:37.208217343 +0000
+--- CouchPotato.py.orig	2015-05-23 00:46:33.149538780 +0000
 +++ CouchPotato.py
-@@ -12,7 +12,7 @@ import sys
+@@ -12,10 +12,11 @@ import sys
  import traceback
  
  # Root path
 -base_path = dirname(os.path.abspath(__file__))
-+base_path = '%%PREFIX%%/share/couchpotato'
++import site;
++base_path = site.getsitepackages()[0]
  
  # Insert local directories into path
- sys.path.insert(0, os.path.join(base_path, 'libs'))
+-sys.path.insert(0, os.path.join(base_path, 'libs'))
++sys.path.insert(0, os.path.join('%%PREFIX%%/share/couchpotato', 'libs'))
+ 
+ from couchpotato.environment import Env
+ from couchpotato.core.helpers.variable import getDataDir, removePyc
