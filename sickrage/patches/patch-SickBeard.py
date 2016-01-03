4b7673a48e9fca9488354d3c3598e08b7432e91a
Jorge Schrauwen <sjorge@blackdot.be>
2016-01-03 16:14:13 +0000

    SickRage v5.0.3

--- sickrage/patches/patch-SickBeard.py
+++ sickrage/patches/patch-SickBeard.py
@@ -1,18 +1,19 @@
 $NetBSD$
 
---- SickBeard.py.orig	2015-10-14 23:36:41.000000000 +0000
+--- SickBeard.py.orig	2015-12-31 12:10:41.000000000 +0000
 +++ SickBeard.py
 @@ -1,4 +1,4 @@
 -#!/usr/bin/env python2.7
 +#!/opt/local/bin/python2.7
+ # -*- coding: utf-8 -*
  # Author: Nic Wolfe <nic@wolfeden.ca>
  # URL: http://code.google.com/p/sickbeard/
- #
-@@ -34,7 +34,10 @@ import subprocess
+@@ -34,7 +34,11 @@ import subprocess
  import traceback
  
  import os
 -sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
++#sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
 +# Force python to load the patched version of cherrypy included with the port,
 +# instead of any version that may be installed otherwise.
 +sys.path.insert(0,'/opt/local/share/sickrage/lib')
@@ -20,12 +21,12 @@ $NetBSD$
  
  import shutil
  import shutil_custom
-@@ -156,7 +159,7 @@ class SickRage(object):
+@@ -151,7 +155,7 @@ class SickRage(object):
          # do some preliminary stuff
-         sickbeard.MY_FULLNAME = os.path.normpath(os.path.abspath(__file__))
-         sickbeard.MY_NAME = os.path.basename(sickbeard.MY_FULLNAME)
--        sickbeard.PROG_DIR = os.path.dirname(sickbeard.MY_FULLNAME)
+         sickbeard.MY_FULLNAME = ek(os.path.normpath, ek(os.path.abspath, __file__))
+         sickbeard.MY_NAME = ek(os.path.basename, sickbeard.MY_FULLNAME)
+-        sickbeard.PROG_DIR = ek(os.path.dirname, sickbeard.MY_FULLNAME)
 +        sickbeard.PROG_DIR = '/opt/local/share/sickrage'
          sickbeard.DATA_DIR = sickbeard.PROG_DIR
          sickbeard.MY_ARGS = sys.argv[1:]
-         sickbeard.SYS_ENCODING = None
+ 
--- sickrage/patches/patch-sickbeard_versionChecker.py
+++ sickrage/patches/patch-sickbeard_versionChecker.py
@@ -1,8 +1,8 @@
 $NetBSD$
 
---- sickbeard/versionChecker.py.orig	2015-10-16 19:41:53.550591789 +0000
+--- sickbeard/versionChecker.py.orig	2015-12-31 12:10:41.000000000 +0000
 +++ sickbeard/versionChecker.py
-@@ -51,13 +51,9 @@ class CheckVersion:
+@@ -51,12 +51,9 @@ class CheckVersion(object):
          self.updater = None
          self.install_type = None
          self.amActive = False
@@ -12,9 +12,9 @@ $NetBSD$
 -                self.updater = GitUpdateManager()
 -            elif self.install_type == 'source':
 -                self.updater = SourceUpdateManager()
- 
++
 +        sickbeard.BRANCH = 'master'
-+        sickbeard.CUR_COMMIT_HASH = 'pkgsrc-%%VERSION%%'
-         self.session=requests.Session()
++        sickbeard.CUR_COMMIT_HASH = 'pkgsrc-5.0.3'
  
-     def run(self, force=False):
+         self.session = requests.Session()
+ 
