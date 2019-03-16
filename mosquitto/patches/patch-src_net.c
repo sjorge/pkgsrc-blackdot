8615761db225fa54fecf9dce93c9a648760e12cf
Jorge Schrauwen <sjorge@acheron.be>
2019-03-16 15:15:42 +0100

    Cleanup some pkglint errors for mosquitto

--- mosquitto/patches/patch-src_net.c
+++ mosquitto/patches/patch-src_net.c
@@ -1,18 +1,21 @@
 $NetBSD$
+Fix the build on SunOS by including the required network headers
 
 --- src/net.c.orig	2019-02-28 17:18:59.000000000 +0000
 +++ src/net.c
-@@ -16,6 +16,9 @@ Contributors:
+@@ -16,6 +16,11 @@ Contributors:
  
  #include "config.h"
  
++#ifdef _SunOS_
 +#include <netinet/in.h>
 +#include <sys/socket.h>
++#endif
 +
  #ifndef WIN32
  #include <netdb.h>
  #include <unistd.h>
-@@ -437,9 +440,10 @@ int net__socket_listen(struct mosquitto_
+@@ -437,9 +442,10 @@ int net__socket_listen(struct mosquitto_
  #ifndef WIN32
  		ss_opt = 1;
  		setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &ss_opt, sizeof(ss_opt));
